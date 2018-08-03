d90a9417bdf489df902621e344ebdd1c0a161751
Terry Ebieto <tebieto@gmail.com>
2018-08-04 00:43:02 +0100

    modified call to algolia search from geocoder 6

--- public/js/app.js
+++ public/js/app.js
@@ -13768,8 +13768,9 @@ var app = new Vue({
 		var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 		geocoder.geocode({ 'latLng': latlng }, function (results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
-				if (results[1]) {
-					address = results[1].formatted_address;
+				console.log(results);
+				if (results[0]) {
+					address = results[0].address_component[4].long_name + ',' + results[0].address_component[5].short_name;
 					if (_this19.place = address) {
 
 						_this19.queryLocation();
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -820,8 +820,9 @@ showPosition(position) {
   var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   geocoder.geocode({'latLng': latlng}, (results, status) => {
     if (status == google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        address = results[1].formatted_address;
+		console.log(results)
+      if (results[0]) {
+        address = results[0].address_component[4].long_name + ',' + results[0].address_component[5].short_name;
         if(this.place = address) {
 			
 			this.queryLocation();
@@ -832,9 +833,7 @@ showPosition(position) {
       }
     }
   });
-  
-
-
+ 
 },
 	
 getLocation() {
